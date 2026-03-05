d85080b8793a4ab60b5f996957ef7be25a2b3685
LOGAVEL M <loguvel158@gmail.com>
2026-03-05 22:39:38 +0530

    Create user_schema.py

--- apps/api/app/schemas/user_schema.py
+++ apps/api/app/schemas/user_schema.py
@@ -16,3 +16,11 @@ class UserResponse(BaseModel):
 
     class Config:
         orm_mode = True
+
+class UserRegister(BaseModel):
+    email: str
+    password: str
+
+class UserLogin(BaseModel):
+    email: str
+    password: str
