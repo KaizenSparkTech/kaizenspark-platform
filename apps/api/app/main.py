6707d11b61e389e61bdbd76cee8645aabb6e6d2c
LOGAVEL M <loguvel158@gmail.com>
2026-03-06 14:21:34 +0530

    Update main.py

--- apps/api/app/main.py
+++ apps/api/app/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
-from app.routers import auth
+from app.routers import auth, project, task
 
 app = FastAPI()
 
 app.include_router(auth.router)
+app.include_router(project.router)
+app.include_router(task.router)
 
 @app.get("/")
 def root():
