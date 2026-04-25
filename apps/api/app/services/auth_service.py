035fa4178d13502364436d6255309f4f6042c9d1
LOGAVEL M <loguvel158@gmail.com>
2026-04-25 13:43:33 +0530

    Update auth_service.py

--- apps/api/app/services/auth_service.py
+++ apps/api/app/services/auth_service.py
@@ -2,23 +2,37 @@ from passlib.context import CryptContext
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+# fake DB (temporary)
+fake_users_db = {}
+
 def hash_password(password: str):
     return pwd_context.hash(password)
 
-def verify_password(password: str, hashed_password: str):
-    return pwd_context.verify(password, hashed_password)
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
 
 def register_user(email: str, password: str):
+    if email in fake_users_db:
+        return {"error": "User already exists"}
+
     hashed_password = hash_password(password)
 
-    return {
-        "message": "User registered successfully",
+    fake_users_db[email] = {
         "email": email,
-        "hashed_password": hashed_password
+        "password": hashed_password
     }
 
+    return {"message": "User registered successfully"}
+
+
 def login_user(email: str, password: str):
-    return {
-        "message": "Login successful",
-        "email": email
-    }
+    user = fake_users_db.get(email)
+
+    if not user:
+        return {"error": "User not found"}
+
+    if not verify_password(password, user["password"]):
+        return {"error": "Invalid password"}
+
+    return {"message": "Login successful"}
